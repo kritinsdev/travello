b92d4beb6c4d1e6c57460742f268994e8b575c71
Kristaps Ritins <kritinsdev@gmail.com>
2023-06-03 02:22:07 +0300

    rm relation manager

--- app/Filament/Resources/PlaceResource.php
+++ app/Filament/Resources/PlaceResource.php
@@ -56,7 +56,6 @@ class PlaceResource extends Resource
     public static function getRelations(): array
     {
         return [
-            RelationManagers\TypeRelationManager::class
         ];
     }
 
